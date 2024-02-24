c7b69d0b6b05e962cc1e1eeecab046c82e081fcc
Daniel Mai <danielmai122001@gmail.com>
2024-02-24 14:55:21 -0600

    tunnel map

---
+++ uom-nav/src/assets/umTunnelsLocation.js
@@ -0,0 +1,398 @@
+[
+    {
+        id: 1,
+        name: 'Arch2',
+        point1: '49.807916,-97.136149',
+        point2: '49.807965,-97.136197',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 2,
+        name: 'E3-Arch2',
+        point1: '49.808413,-97.134692',
+        point2: '49.807916,-97.136149',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 3,
+        name: 'E3',
+        point1: '49.808479,-97.13473',
+        point2: '49.808413,-97.134692',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 4,
+        name: 'E3',
+        point1: '49.808595,-97.134325',
+        point2: '49.808479,-97.13473',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 5,
+        name: 'to Admin',
+        point1: '49.809093,-97.133606',
+        point2: '49.809391,-97.13259',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 6,
+        name: 'to E2',
+        point1: '49.809185,-97.133676',
+        point2: '49.808898,-97.133467',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 7,
+        name: 'E2-E3',
+        point1: '49.808898,-97.133467',
+        point2: '49.808595,-97.134325',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 8,
+        name: 'E3-UC',
+        point1: '49.808595,-97.134325',
+        point2: '49.809246,-97.134789',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 9,
+        name: 'Hellen Glass',
+        point1: '49.809332,-97.135693',
+        point2: '49.809209,-97.135621',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 10,
+        name: 'UC-Hellen Glass',
+        point1: '49.809209,-97.135621',
+        point2: '49.809261,-97.135417',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 11,
+        name: 'UC',
+        point1: '49.809261,-97.135417',
+        point2: '49.809112,-97.135304',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 12,
+        name: 'UC',
+        point1: '49.809112,-97.135304',
+        point2: '49.809246,-97.134789',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 13,
+        name: 'UC',
+        point1: '49.809246,-97.134789',
+        point2: '49.809452,-97.134156',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 14,
+        name: 'UC',
+        point1: '49.809452,-97.134156',
+        point2: '49.809735,-97.13411',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 15,
+        name: 'UC-north',
+        point1: '49.809735,-97.13411',
+        point2: '49.810237,-97.134204',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 16,
+        name: 'to BSB',
+        point1: '49.810237,-97.134204',
+        point2: '49.810168,-97.134486',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 17,
+        name: 'Fitzgerald to BSB',
+        point1: '49.810113,-97.134473',
+        point2: '49.81026,-97.13451',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 18,
+        name: 'south-Allen',
+        point1: '49.810237,-97.134204',
+        point2: '49.81071,-97.134282',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 19,
+        name: 'Allen-Armes',
+        point1: '49.81071,-97.134282',
+        point2: '49.81086,-97.134038',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 20,
+        name: 'Armes-Wallce',
+        point1: '49.81086,-97.134038',
+        point2: '49.811712,-97.134888',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 21,
+        name: 'Wallice',
+        point1: '49.811712,-97.134888',
+        point2: '49.811819,-97.136382',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 22,
+        name: 'Armes-Machray',
+        point1: '49.81086,-97.134038',
+        point2: '49.811158,-97.133121',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 23,
+        name: 'Machray-Duff Roblin',
+        point1: '49.811158,-97.133121',
+        point2: '49.810815,-97.132807',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 24,
+        name: 'Duff Roblin-Buller',
+        point1: '49.810815,-97.132807',
+        point2: '49.810734,-97.132732',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 25,
+        name: 'Buller',
+        point1: '49.810734,-97.132732',
+        point2: '49.810416,-97.133689',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 26,
+        name: 'Duff Roblin',
+        point1: '49.810815,-97.132807',
+        point2: '49.811155,-97.131841',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 27,
+        name: 'HumanEcology??',
+        point1: '49.810982,-97.132365',
+        point2: '49.810767,-97.132193',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 28,
+        name: 'Duff Roblin-University College',
+        point1: '49.811155,-97.131841',
+        point2: '49.811018,-97.131688',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 29,
+        name: 'University College',
+        point1: '49.811018,-97.131688',
+        point2: '49.811444,-97.130444',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 30,
+        name: 'University College-Robson Hall',
+        point1: '49.811444,-97.130444',
+        point2: '49.811793,-97.130704',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 31,
+        name: 'University College-Dafoe',
+        point1: '49.811018,-97.131688',
+        point2: '49.810545,-97.131281',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 32,
+        name: 'Dafoe',
+        point1: '49.810545,-97.131281',
+        point2: '49.810438,-97.131694',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 33,
+        name: 'Dafoe-Fletcher',
+        point1: '49.810438,-97.131694',
+        point2: '49.80969,-97.131058',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 34,
+        name: 'Fletcher',
+        point1: '49.809599,-97.131291',
+        point2: '49.809784,-97.130763',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 35,
+        name: 'Fletcher-Isbister',
+        point1: '49.809784,-97.130763',
+        point2: '49.809164,-97.130245',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 36,
+        name: 'Fletcher-Drake',
+        point1: '49.809599,-97.131291',
+        point2: '49.808115,-97.130079',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 37,
+        name: 'Drake',
+        point1: '49.808115,-97.130079',
+        point2: '49.807942,-97.130627',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 38,
+        name: 'Animal Science - Agriculture',
+        point1: '49.806313, -97.136893',
+        point2: '49.806592, -97.135998',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 39,
+        name: 'Animal Science-Agriculture Lecture Hall - Agriculture',
+        point1: '49.806382, -97.136772',
+        point2: '49.806354, -97.136751',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 40,
+        name: 'Animal Science - Agriculture',
+        point1: '49.806240, -97.137378',
+        point2: '49.806313, -97.136893',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 41,
+        name: 'FK',
+        point1: '49.808018,-97.136304',
+        point2: '49.807097,-97.13919',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 42,
+        name: 'FK - Max Bell',
+        point1: '49.807404,-97.138366',
+        point2: '49.808366,-97.139229',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 43,
+        name: 'Arch2-FK',
+        point1: '',
+        point2: '',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    },
+    {
+        id: 44,
+        name: 'FK',
+        point1: '49.807211,-97.138714',
+        point2: '49.806812,-97.138381',
+        layer: 13,
+        keywords: '',
+        campus: 'fortgarry'
+    }
+];
