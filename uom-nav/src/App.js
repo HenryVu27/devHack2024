c72a48c95c1c09584d82f8cfdb4ccd51b6320c73
Daniel Mai <danielmai122001@gmail.com>
2024-02-24 01:14:37 -0600

    fix bug = Jack

--- uom-nav/src/App.js
+++ uom-nav/src/App.js
@@ -69,30 +69,13 @@ export default function App() {
 
     return (
         <MapContainer center={[49.8092, -97.13]} zoom={13}>
-            {/* OPEN STREEN MAPS TILES */}
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {/* WATERCOLOR CUSTOM TILES */}
-            {/* <TileLayer
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-      /> */}
-            {/* GOOGLE MAPS TILES */}
-            {/* <TileLayer
-        attribution="Google Maps"
-        // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-        url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-        maxZoom={20}
-        subdomains={["mt0", "mt1", "mt2", "mt3"]}
-      /> */}
 
-<<<<<<< HEAD
             <AddMarkerToClickLocation />
             <SelfLocation />
-=======
             <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
                 {/* Mapping through the markers */}
                 {markers.map((marker, idx) => (
@@ -100,20 +83,7 @@ export default function App() {
                         <Popup>{marker.popUp}</Popup>
                     </Marker>
                 ))}
-
-                {/* Hard coded markers */}
-                {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
-          <Popup>This is popup 1</Popup>
-        </Marker>
-        <Marker position={[51.504, -0.1]} icon={customIcon}>
-          <Popup>This is popup 2</Popup>
-        </Marker>
-        <Marker position={[51.5, -0.09]} icon={customIcon}>
-          <Popup>This is popup 3</Popup>
-        </Marker>
-       */}
             </MarkerClusterGroup>
->>>>>>> 5c90eb9418bd6e4f0147e4da58e01a5d27433f38
         </MapContainer>
     );
 }
